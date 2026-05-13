d2dc2df674ec2748f6f230b2cdce86979265efe4
emmikapk-bit <emmika.pk@gmail.com>
2026-05-13 14:53:19 +0700

    first_commit_backend

---
+++ src/fakeData/fakeUsers.js
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    ID: "1",
+    username: "Emmika",
+    email: "Emmika@gmail.com",
+    passwrod: "Em12345",
+  },
+  {
+    ID: "2",
+    username: "Emmi",
+    email: "Emmi@gmail.com",
+    passwrod: "mi12345",
+  },
+  {
+    ID: "3",
+    username: "Em",
+    email: "Em@gmail.com",
+    passwrod: "ka12345",
+  },
+];
