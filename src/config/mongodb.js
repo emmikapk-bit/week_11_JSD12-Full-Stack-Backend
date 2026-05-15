f656e41ce634cc43453c2ae6f35ec8ff31dd478d
emmikapk-bit <emmika.pk@gmail.com>
2026-05-15 15:28:34 +0700

    added_connected_mongoDB

---
+++ src/config/mongodb.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export async function connectDB() {
+  const uri = process.env.MONGODB_URI;
+
+  try {
+    await mongoose.connect(uri, { dbName: "jsd12-express-app" });
+    console.log("MongoDB connected ✅");
+  } catch (err) {
+    console.log("MongoDB connection error ❌", err);
+
+    throw err;
+  }
+}
